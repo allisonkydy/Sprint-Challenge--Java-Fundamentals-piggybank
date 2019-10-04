5da355c5089c600fcd3de1ecff39e79c9b980b91
allisonkydy <allisnelly@gmail.com>
2019-10-04 10:11:10 -0500

    add fields, contructors, methods to Coin

--- PiggyBank/src/piggybank/Coin.java
+++ PiggyBank/src/piggybank/Coin.java
@@ -2,5 +2,44 @@ package piggybank;
 
 public abstract class Coin
 {
-  
+  // fields
+  protected double value;
+  protected int amount;
+
+  // constructor
+  public Coin(double value, int amount)
+  {
+    this.value = value;
+    this.amount = amount;
+  }
+
+  public Coin(double value)
+  {
+    this.value = value;
+    amount = 1;
+  }
+
+  // abstract methods
+  public abstract void printTotal();
+
+  // getters and setters
+  public double getValue()
+  {
+    return value;
+  }
+
+  public void setValue(double value)
+  {
+    this.value = value;
+  }
+
+  public int getAmount()
+  {
+    return amount;
+  }
+
+  public void setAmount(int amount)
+  {
+    this.amount = amount;
+  }
 }
