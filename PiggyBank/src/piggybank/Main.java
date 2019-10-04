0ed3a16d24410c8a6d262d8a099e5cac81d264d5
allisonkydy <allisnelly@gmail.com>
2019-10-04 13:05:16 -0500

    attempted stretch -- subtract method returns correct total value

--- PiggyBank/src/piggybank/Main.java
+++ PiggyBank/src/piggybank/Main.java
@@ -2,9 +2,52 @@ package piggybank;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Main
 {
+  public static double subtract(ArrayList<Coin> arr, double totalValue, double subtractValue)
+  {
+    double leftToSubtract = subtractValue;
+    for (Coin c : arr)
+    // for(Iterator<Coin> iterator = arr.iterator(); iterator.hasNext();)
+    {
+      // Coin c = iterator.next();
+      // exit loop if there is nothing left to subtract
+      if (leftToSubtract == 0)
+      {
+        break;
+      }
+
+      // if the coin's is less than or equal to the remaining subtract value
+      if (c.getValue() <= leftToSubtract)
+      {
+        // for each coin in the coin's amount
+        for (int i = 0, l = c.getAmount(); i < l; i++)
+        {
+          // exit loop if coin's value is greater than the remaining subtract value
+          if (Math.round(c.getValue() * 100.0) / 100.0 > Math.round(leftToSubtract * 100.0) / 100.0)
+          {
+            break;
+          }
+          
+          // throws error: 
+          // if (i == c.getAmount() - 1)
+          // {
+              // arr.remove(c);
+          //   iterator.remove();
+          // }
+
+          // subtract the coin's value from the remaining subtract value
+          leftToSubtract -= Math.round(c.getValue() * 100.0) / 100.0;
+        }
+      }
+    }
+
+    // return updated total value
+    return (totalValue - (subtractValue - leftToSubtract));
+  }
+
   public static void main(String[] args)
   {
     // decimal formatting
@@ -26,11 +69,16 @@ public class Main
     piggyBank.forEach(coin -> coin.printAmount());
 
     // print value of piggybank
-    double totalValue = 0;
+    double totalValue = 0.00;
     for(int i = 0; i < piggyBank.size(); i++)
     {
       totalValue += piggyBank.get(i).getTotalValue();
     }
     System.out.println("The piggy bank holds " + fp.format(totalValue));
+
+    // remove $1.50 from the piggy bank
+    double updatedTotalValue = subtract(piggyBank, totalValue, 1.50);
+
+    System.out.println("The piggy bank holds " + fp.format(updatedTotalValue));
   }
 }
