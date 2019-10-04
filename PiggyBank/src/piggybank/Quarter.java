3e9c7531cec6915bce627c4c35a28503f11c0a00
allisonkydy <allisnelly@gmail.com>
2019-10-04 10:55:15 -0500

    add constructors and method to Quarter

--- PiggyBank/src/piggybank/Quarter.java
+++ PiggyBank/src/piggybank/Quarter.java
@@ -1,6 +1,31 @@
 package piggybank;
 
-public class Quarter
+public class Quarter extends Coin
 {
-  
+  // constructor
+  public Quarter(int amount)
+  {
+    super(amount);
+    this.value = 0.25;
+  }
+
+  public Quarter()
+  {
+    super();
+    this.value = 0.25;
+  }
+
+  // methods
+  @Override 
+  public void printAmount()
+  {
+    if (amount == 1)
+    {
+      System.out.println(amount + " Quarter");
+    }
+    else
+    {
+      System.out.println(amount + " Quarters");
+    }
+  }
 }
