4b07f7a74f907f3fff96da4db7ebeb643d6f3da5
allisonkydy <allisnelly@gmail.com>
2019-10-04 10:54:21 -0500

    add constructor and method to Penny

--- PiggyBank/src/piggybank/Penny.java
+++ PiggyBank/src/piggybank/Penny.java
@@ -1,6 +1,31 @@
 package piggybank;
 
-public class Penny
+public class Penny extends Coin
 {
-  
+  // constructor
+  public Penny(int amount)
+  {
+    super(amount);
+    this.value = 0.01;
+  }
+
+  public Penny()
+  {
+    super();
+    this.value = 0.01;
+  }
+
+  // methods
+  @Override 
+  public void printAmount()
+  {
+    if (amount == 1)
+    {
+      System.out.println(amount + " Penny");
+    }
+    else
+    {
+      System.out.println(amount + " Pennies");
+    }
+  }
 }
