66f5fc5021804a7f8696dd7f9a760e0318c53386
allisonkydy <allisnelly@gmail.com>
2019-10-04 11:02:30 -0500

    print contents of piggybank

--- PiggyBank/src/piggybank/Main.java
+++ PiggyBank/src/piggybank/Main.java
@@ -1,17 +1,28 @@
 package piggybank;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 public class Main
 {
   public static void main(String[] args)
   {
+    // decimal formatting
     DecimalFormat fp = new DecimalFormat("$###,###.00");
 
-    // Nickel n = new Nickel(2);
-    // System.out.println(fp.format(n.getValue()));
-    // System.out.println(n.getAmount());
-    // n.printTotalValue();
-    // n.printAmount();
+    // create collection for piggybank
+    ArrayList<Coin> piggyBank = new ArrayList<Coin>();
+
+    // add coins to piggybank
+    piggyBank.add(new Quarter());
+    piggyBank.add(new Dime());
+    piggyBank.add(new Dollar(5));
+    piggyBank.add(new Nickel(3));
+    piggyBank.add(new Dime(7));
+    piggyBank.add(new Dollar());
+    piggyBank.add(new Penny(10));
+
+    // print contents of piggybank
+    piggyBank.forEach(coin -> coin.printAmount());
   }
 }
